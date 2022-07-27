ff30fb299a3a9305c8fe6fe4408f9e9e6eb93c97
Richino <archerrichino@gmail.com>
2022-07-27 01:15:14 -0400

    changes made

--- server/amazon.js
+++
@@ -1,8 +0,0 @@
-const AWS = require("aws-sdk");
-const s3 = new AWS.S3({
-    region: process.env.REGION,
-    accessKeyId: process.env.ACCESSID,
-    secretAccessKey: process.env.SECRETACCESSKEY,
-});
-
-module.exports = s3
